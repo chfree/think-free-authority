1c1f7f29fcb5e4f3b75fcf042edc4972e11f5466
zfree <79763939@qq.com>
2019-07-15 00:11:52 +0800

    加入了swagger，目前没有太好看的swagger-ui可以供使用，打算自己写一个swagger-ui

--- authority-web-starter-run/src/main/java/com/tennetcn/free/authority/run/AuthorityDemoApp.java
+++ authority-web-starter-run/src/main/java/com/tennetcn/free/authority/run/AuthorityDemoApp.java
@@ -2,6 +2,7 @@ package com.tennetcn.free.authority.run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author chfree
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableSwagger2
 public class AuthorityDemoApp {
     public static void main(String[] args) {
         SpringApplication.run(AuthorityDemoApp.class,args);
