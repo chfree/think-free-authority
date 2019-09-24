3ebaf7620b3ab67bcb5c1018d91cfb3bfda04556
zfree <79763939@qq.com>
2019-09-24 12:44:23 +0800

    添加了login模块；依赖了base的maange，去掉了版本号

---
+++ authority-login-starter/src/main/java/com/tennetcn/free/authority/apimodel/login/LoginReq.java
@@ -0,0 +1,21 @@
+package com.tennetcn.free.authority.apimodel.login;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @author chfree
+ * @email chfree001@gmail.com
+ * @create 2019-07-25 15:57
+ * @comment
+ */
+
+@Data
+public class LoginReq {
+    @NotBlank(message="用户名不能为空")
+    private String username;
+
+    @NotBlank(message="密码不能为空")
+    private String password;
+}
---
+++ authority-login-starter/src/main/java/com/tennetcn/free/authority/autoconfig/LoginAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.tennetcn.free.authority.autoconfig;
+
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @author chfree
+ * @email chfree001@gmail.com
+ * @create 2019-07-12 00:01
+ * @comment
+ */
+
+@ImportResource(locations={"login-starter-config.xml"})
+public class LoginAutoConfiguration {
+}
