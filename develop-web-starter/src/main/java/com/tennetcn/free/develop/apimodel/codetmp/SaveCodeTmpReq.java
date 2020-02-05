da4a29943e349de396a4f73814bead947d3821c6
zfree <79763939@qq.com>
2020-02-05 23:04:06 +0800

    基础的增删改查功能

---
+++ develop-web-starter/src/main/java/com/tennetcn/free/develop/apimodel/codetmp/SaveCodeTmpReq.java
@@ -0,0 +1,20 @@
+package com.tennetcn.free.develop.apimodel.codetmp;
+
+import com.tennetcn.free.develop.model.CodeTmp;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @author      auto build code by think
+ * @email       chfree001@gmail.com
+ * @createtime  2020-02-05 16:28:45
+ * @comment     代码模板
+ */
+
+@Data
+public class SaveCodeTmpReq extends CodeTmp {
+
+    @NotBlank(message = "模板名称为必填项")
+    private String name;
+}
---
+++ develop-web-starter/src/main/java/com/tennetcn/free/develop/apis/CodeTmpApi.java
@@ -0,0 +1,87 @@
+package com.tennetcn.free.develop.apis;
+
+
+import cn.hutool.core.util.IdUtil;
+import com.tennetcn.free.core.enums.ModelStatus;
+import com.tennetcn.free.core.message.web.BaseResponse;
+import com.tennetcn.free.develop.apimodel.codetmp.CodeTmpListReq;
+import com.tennetcn.free.develop.apimodel.codetmp.CodeTmpListResp;
+import com.tennetcn.free.develop.apimodel.codetmp.SaveCodeTmpReq;
+import com.tennetcn.free.develop.model.CodeTmp;
+import com.tennetcn.free.develop.service.ICodeTmpService;
+import com.tennetcn.free.develop.viewmodel.CodeTmpSearch;
+import com.tennetcn.free.security.webapi.AuthorityApi;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
+
+/**
+ * @author      auto build code by think
+ * @email       chfree001@gmail.com
+ * @createtime  2020-02-05 16:46:13
+ * @comment     代码模板
+ */
+@RestController
+@RequestMapping(value = "/api/v1/develop/codeTmp/",produces =  MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Api(tags="代码模板管理",value ="代码模板相关的操作")
+public class CodeTmpApi extends AuthorityApi {
+    @Autowired
+    ICodeTmpService codeTmpService;
+
+    @ApiOperation(value = "获取代码模板列表")
+    @PostMapping("list")
+    public BaseResponse list(@RequestBody @Valid CodeTmpListReq listReq){
+        CodeTmpListResp resp = new CodeTmpListResp();
+        resp.setTotalCount(codeTmpService.queryCountBySearch(listReq.getSearch()));
+        resp.setCodeTmps(codeTmpService.queryListBySearch(listReq.getSearch(),listReq.getPager()));
+
+        return resp;
+    }
+
+    @ApiOperation(value = "获取指定代码模板")
+    @GetMapping("get")
+    public BaseResponse get(@Valid @NotBlank(message = "代码模板id不能为空") String id){
+        BaseResponse response=new BaseResponse();
+
+        CodeTmp codeTmp = codeTmpService.queryModel(id);
+        response.put("codeTmp",codeTmp);
+
+        return response;
+    }
+
+    @ApiOperation(value = "搜索代码模板数量")
+    @PostMapping("countSearch")
+    public BaseResponse countSearch(CodeTmpSearch search){
+        BaseResponse response=new BaseResponse();
+
+        int count =  codeTmpService.queryCountBySearch(search);
+        response.put("count",count);
+
+        return response;
+    }
+
+    @ApiOperation(value = "保存一个代码模板")
+    @PostMapping("save")
+    public BaseResponse save(@Valid SaveCodeTmpReq saveCodeTmpReq){
+        BaseResponse response = new BaseResponse();
+        if(StringUtils.isEmpty(saveCodeTmpReq.getId())){
+            saveCodeTmpReq.setId(IdUtil.randomUUID());
+            saveCodeTmpReq.setCreateUserId(getLoginId());
+            saveCodeTmpReq.setModelStatus(ModelStatus.add);
+        }else{
+            saveCodeTmpReq.setModelStatus(ModelStatus.update);
+        }
+
+        boolean result = codeTmpService.applyChange(saveCodeTmpReq);
+        response.put("result",result);
+        return response;
+    }
+
+}
