b0ce65374680a84ac1e1d19fe60418062779f11e
zfree <79763939@qq.com>
2021-08-24 21:33:20 +0800

    param项目加入starter

--- param/param-data-service-starter/src/main/java/com/cditer/free/param/autoconfig/ParamLogicalAutoConfiguration.java
+++ param/param-data-service-starter/src/main/java/com/cditer/free/param/autoconfig/ParamLogicalAutoConfiguration.java
@@ -1,4 +1,10 @@
 package com.cditer.free.param.autoconfig;
 
+import org.springframework.context.annotation.ImportResource;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@MapperScan(value = "com.cditer.free.param.logical.mapper")
+@ImportResource(locations={"param-logical-starter-config.xml"})
 public class ParamLogicalAutoConfiguration {
+
 }
--- param/param-web-starter/src/main/java/com/cditer/free/param/autoconfig/ParamWebAutoConfiguration.java
+++ param/param-web-starter/src/main/java/com/cditer/free/param/autoconfig/ParamWebAutoConfiguration.java
@@ -1,4 +1,7 @@
 package com.cditer.free.param.autoconfig;
 
+import org.springframework.context.annotation.ImportResource;
+
+@ImportResource(locations={"param-web-starter-config.xml"})
 public class ParamWebAutoConfiguration {
 }
